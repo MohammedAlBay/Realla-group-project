d7e47e94fc74b52f76526be230610bc5574edd81
FundaErmete <funda.ermete@gmail.com>
2024-03-15 15:28:37 +0100

    Add About Page

--- resources/js/Layouts/Partials/Footer.jsx
+++ resources/js/Layouts/Partials/Footer.jsx
@@ -1,12 +1,35 @@
+// resources/js/components/Footer.js
+
 import React from 'react';
 
-function Footer() {
-    return (
-        <footer>
-            {/* Your footer content goes here */}
-            <p>&copy; {new Date().getFullYear()} My Website</p>
-        </footer>
-    );
+const Footer = () => {
+  return (
+    <footer>
+      {/* Subscription Section */}
+      <div className="subscribe">
+        <input type="email" placeholder="Enter your email" />
+        <button>Subscribe</button>
+      </div>
+      
+      {/* Logo Section */}
+      <div className="logo">
+      <img src="./images/realla_logo.png" className="logo" alt="Logo"/>
+      </div>
+      
+      {/* Links Section */}
+      <ul className="footer-links">
+        <li><a href="#">Link 1</a></li>
+        <li><a href="#">Link 2</a></li>
+        <li><a href="#">Link 3</a></li>
+        {/* Add more links as needed */}
+      </ul>
+      
+      {/* Copyright */}
+      <div className="copyright">
+        <p>&copy; 2024 Realla. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
---
+++ resources/js/Pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'boxicons/css/boxicons.min.css';
+
+
+const About = () => {
+    return (
+        <div className="galleryPage">
+            <h1>this is test of about page</h1>
+        </div>
+    );
+};
+
+export default About;
