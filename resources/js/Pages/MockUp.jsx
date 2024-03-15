0035820a966de3d8537285cf6789d15130abd286
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-15 15:48:25 +0100

    update mockup component

--- resources/js/Pages/MockUp.jsx
+++ resources/js/Pages/MockUp.jsx
@@ -3,11 +3,11 @@ import 'boxicons/css/boxicons.min.css';
 import MockUp1 from '@/Pages/MockUp1.jsx'
 
 
-function MockUp1() {
+function MockUp() {
     return (
             <MockUp1 />
     );
 }
 
-export default MockUp1;
+export default MockUp;
 
