c6cb211f7be4dd7d26c61c6b5ba898600a4fb9d2
Jana2xN <jana.neirinck@hotmail.com>
2024-03-18 15:57:59 +0100

    Add form in const LoginPanel

--- resources/js/Pages/LoginPanel.jsx
+++ resources/js/Pages/LoginPanel.jsx
@@ -21,43 +21,7 @@ const LoginPanel = () => {
             </div>
             <div className="flex-grow">
                 <h2 className="text-2xl font-semibold mb-4">Welcome Back to REALLA!</h2>
-                <p>Sign in your account</p>
-                <form onSubmit={handleSubmit} className="mt-4">
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="Your Email"
-                        className="border rounded-lg p-2 mt-2"
-                    />
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="Password"
-                        className="border rounded-lg p-2 mt-2"
-                    />
-                    <input
-                        type="password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                        placeholder="Confirm Password"
-                        className="border rounded-lg p-2 mt-2"
-                    />
-                    <div className="flex items-center justify-between mb-4">
-                        <div className="flex items-center">
-                            <input
-                                type="checkbox"
-                                checked={isLandlord}
-                                onChange={(e) => setIsLandlord(e.target.checked)}
-                                className="mr-2"
-                            />
-                            <label>Remember Me</label>
-                        </div>
-                        <a href="#" className="text-white">Forgot Password?</a>
-                    </div>
-                    <button type="submit" className="bg-yellow-500 text-gray-900 py-2 px-4 rounded-lg">Login</button>
-                </form>   
+                <p>Sign in your account</p>   
                 <p className="mt-4">Don’t have an account? <a href="#" className="text-yellow-500">Register</a></p>
             </div>
         </div>
