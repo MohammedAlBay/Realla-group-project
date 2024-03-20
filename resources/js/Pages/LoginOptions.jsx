0d23ab11fa36d04fdc5b448a45499d911b9c9d8f
Jana2xN <jana.neirinck@hotmail.com>
2024-03-20 14:45:10 +0100

    Add rounded rectangle to register.jsx

--- resources/js/Pages/LoginOptions.jsx
+++ resources/js/Pages/LoginOptions.jsx
@@ -4,11 +4,13 @@ import { Link } from '@inertiajs/react';
 const LoginOptions = () => {
     return (
         <div className="flex bg-gray-900 text-white p-8 pt-6 h-screen" style={{ backgroundColor: '#292F36' }}> {/* Increased margin-top to 6 and set height to full screen */}
+            <div className="relative w-2/3 ml-52" style={{ marginTop: '-4rem' }}> {/* Set width to 2/3 of the page and added margin to push image more to the left */}
+                <div style={{ position: 'relative', width: '500px', height: '625px', flexShrink: 0, borderRadius: '0px 0px 100px 100px', border: '3px solid rgba(0, 0, 0, 0.00)', background: '#1E2127' }}></div>
+            </div>            
+            <div className="absolute inset-0 flex justify-center items-center" style={{ marginTop: '8rem' }}> {/* Positioned the image lower */}
+                <img src="/images/LoginOptionsImage.png" alt="LoginOptions" className="object-cover w-2/3 h-2/3" /> {/* Made the image smaller */}
+            </div>
             <div className="relative w-2/3 mr-20"> {/* Set width to 2/3 of the page and added margin to push image more to the left */}
-                <div className="absolute inset-0 flex justify-center items-center">
-                <div style={{ position: 'relative', top: '2rem', width: '500px', height: '625px', flexShrink: 0, borderRadius: '0px 0px 100px 100px', border: '3px solid rgba(0, 0, 0, 0.00)', background: '#1E2127' }}></div>
-                    <img src="/images/LoginOptionsImage.png" alt="LoginOptions" className="absolute inset-0 object-cover w-full h-full" />
-                </div>
             </div>
             <div className="flex justify-center items-center"> {/* Centered vertically and horizontally */}
                 <div className="flex flex-col justify-center items-center"> {/* Centered vertically and horizontally */}
--- resources/js/Pages/Register.jsx
+++ resources/js/Pages/Register.jsx
@@ -21,6 +21,9 @@ const Register = () => {
 
     return (
         <div className="flex bg-gray-900 text-white p-8 justify-between" style={{ backgroundColor: '#292F36' }}> {/* Space between items */}
+            <div className="relative w-2/3 ml-52" style={{ marginTop: '-4rem' }}> {/* Set width to 2/3 of the page and added margin to push image more to the left */}
+                <div style={{ position: 'relative', width: '500px', height: '625px', flexShrink: 0, borderRadius: '0px 0px 100px 100px', border: '3px solid rgba(0, 0, 0, 0.00)', background: '#1E2127' }}></div>
+            </div>
             <div className="w-4/5 max-w-md ml-auto mr-20 mt-32 mb-20"> {/* Push to the right with ml-auto, and a bit to the left with mr-4 */}
                 <div className="relative mr-8">
                     <div className="absolute inset-0"></div>
