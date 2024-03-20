aeca951ccc358d5c4f4733e0ba5debe1ab9f125f
Jana2xN <jana.neirinck@hotmail.com>
2024-03-20 12:17:38 +0100

    Positions buttons in LoginOptions.jsx so they are center next to the image

--- resources/js/Pages/LoginOptions.jsx
+++ resources/js/Pages/LoginOptions.jsx
@@ -3,16 +3,16 @@ import { Link } from '@inertiajs/react';
 
 const LoginOptions = () => {
     return (
-        <div className="flex bg-gray-900 text-white p-8">
-            <div className="relative mr-8">
-                <div className="absolute inset-0 bg-gray-700 rounded-lg border-2 border-transparent"></div>
-                <img src="./LoginOptionsImage.png" alt="LoginOptions" className="absolute inset-0 object-cover w-full h-full" />
+        <div className="flex bg-gray-900 text-white p-8 pt-10 h-screen"> {/* Increased margin-top to 10 and set height to full screen */}
+            <div className="relative w-2/3 mr-20"> {/* Set width to 2/3 of the page and added margin to push image more to the left */}
+                <div className="absolute inset-0">
+                    <img src="/images/LoginOptionsImage.png" alt="LoginOptions" className="absolute inset-0 object-cover w-full h-full" />
+                </div>
             </div>
-            <div className="flex flex-col justify-between">
-                <div className="flex flex-col"> {/* Adjusted to display buttons vertically */}
-                    <button className="w-1962 h-223 flex-shrink-0 rounded-lg border-3 border-transparent bg-yellow-500 text-gray-900 py-4 px-8 mb-4">Login as Landlord</button>
-                    <div style={{ marginBottom: '20px' }}></div> {/* Added space between buttons */}
-                    <button className="w-1962 h-223 flex-shrink-0 rounded-lg border-3 border-transparent bg-yellow-500 text-gray-900 py-4 px-8">Login as Tenant</button>
+            <div className="flex justify-center items-center"> {/* Centered vertically and horizontally */}
+                <div className="flex flex-col justify-center items-center"> {/* Centered vertically and horizontally */}
+                    <button className="w-72 h-12 rounded-lg border-3 border-transparent bg-yellow-500 text-gray-900 py-2 px-4 mb-4">Login as Landlord</button>
+                    <button className="w-72 h-12 rounded-lg border-3 border-transparent bg-yellow-500 text-gray-900 py-2 px-4">Login as Tenant</button>
                 </div>
             </div>
         </div>
@@ -20,4 +20,3 @@ const LoginOptions = () => {
 };
 
 export default LoginOptions;
-
