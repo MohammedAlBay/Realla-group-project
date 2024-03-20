2842db84f24e995d7ee5df2bb24ccd8bcba6eb8a
MohammedAB <albay.m@hotmail.com>
2024-03-20 17:07:49 +0100

    Update styling of socialmedia icons

--- resources/js/Components/SocialMediaLinks.jsx
+++ resources/js/Components/SocialMediaLinks.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 function SocialMediaLinks() {
   return (
     <ul className="social-media-links">
-      <li><a href="#"><i className='bx bxl-facebook-circle'></i></a></li>
-      <li><a href="#"><i className='bx bxl-instagram-alt'></i></a></li>
-      <li><a href="#"><i className='bx bxl-linkedin'></i></a></li>
+      <li><a href="#"><i class='bx bxl-facebook-circle' style={{color:'#fdb514'}}></i></a></li>
+      <li><a href="#"><i class='bx bxl-instagram-alt' style={{color:'#fdb514'}}></i></a></li>
+      <li><a href="#"><i class='bx bxl-linkedin' style={{color:'#fdb514'}}></i></a></li>
     </ul>
   );
 }
