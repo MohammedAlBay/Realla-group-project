689bd9b6aab0017ca905f54e09c7f75a13aa3109
Jana2xN <jana.neirinck@hotmail.com>
2024-03-20 14:02:10 +0100

    Add rounded rectangle to LoginOption.jsx and place behind image

--- resources/js/Pages/LoginOptions.jsx
+++ resources/js/Pages/LoginOptions.jsx
@@ -3,9 +3,10 @@ import { Link } from '@inertiajs/react';
 
 const LoginOptions = () => {
     return (
-        <div className="flex bg-gray-900 text-white p-8 pt-10 h-screen"> {/* Increased margin-top to 10 and set height to full screen */}
+        <div className="flex bg-gray-900 text-white p-8 pt-6 h-screen" style={{ backgroundColor: '#292F36' }}> {/* Increased margin-top to 6 and set height to full screen */}
             <div className="relative w-2/3 mr-20"> {/* Set width to 2/3 of the page and added margin to push image more to the left */}
-                <div className="absolute inset-0">
+                <div className="absolute inset-0 flex justify-center items-center">
+                <div style={{ position: 'relative', top: '2rem', width: '500px', height: '625px', flexShrink: 0, borderRadius: '0px 0px 100px 100px', border: '3px solid rgba(0, 0, 0, 0.00)', background: '#1E2127' }}></div>
                     <img src="/images/LoginOptionsImage.png" alt="LoginOptions" className="absolute inset-0 object-cover w-full h-full" />
                 </div>
             </div>
