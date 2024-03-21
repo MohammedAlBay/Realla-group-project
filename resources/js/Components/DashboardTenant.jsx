0e4c7b979af8024ef7c6e0c1647c9dc28969c0b9
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-21 16:53:07 +0100

    change the color of icons

--- resources/js/Components/DashboardTenant.jsx
+++ resources/js/Components/DashboardTenant.jsx
@@ -6,38 +6,38 @@ const DashboardTenant = () => {
                 <ul className="sidebar-menu">
                     <li className="sidebar-menu-item">
                     <div className="sidebar-menu-content">
-                        <i className='bx bxs-compass'  style={{ color: '#d2971b'}} />
+                        <i className='bx bxs-compass'  style={{ color: '#FDB414'}} />
                         <a href="#" className="sidebar-menu-link">Explore</a>
                     </div>
                     </li>
 
                     <li className="sidebar-menu-item">
                         <div className="sidebar-menu-content">
-                            <i className='bx bxs-hand' style={{ color: '#d2971b'}}></i>
+                            <i className='bx bxs-hand' style={{ color: '#FDB414'}}></i>
                             <a href="#" className="sidebar-menu-link">Report a problem</a>
                         </div>
                     </li>
                     <li className="sidebar-menu-item">
                         <div className="sidebar-menu-content">
-                            <i className='bx bxs-message-detail' style={{ color: '#d2971b'}}></i>
+                            <i className='bx bxs-message-detail' style={{ color: '#FDB414'}}></i>
                             <a href="#" className="sidebar-menu-link">Message</a>
                         </div>
                     </li>
                     <li className="sidebar-menu-item">
                         <div className="sidebar-menu-content">
-                            <i className='bx bx-calendar' style={{ color: '#d2971b'}}></i>
+                            <i className='bx bx-calendar' style={{ color: '#FDB414'}}></i>
                             <a href="#" className="sidebar-menu-link">Appointment</a>
                         </div>
                     </li>
                     <li className="sidebar-menu-item">
                         <div className="sidebar-menu-content">
-                            <i className='bx bx-folder-open' style={{ color: '#d2971b'}}></i>
+                            <i className='bx bx-folder-open' style={{ color: '#FDB414'}}></i>
                             <a href="#" className="sidebar-menu-link">Documents</a>
                         </div>
                     </li>
                     <li className="sidebar-menu-item">
                         <div className="sidebar-menu-content">
-                            <i className='bx bxs-user-circle' style={{ color: '#d2971b'}} ></i>
+                            <i className='bx bxs-user-circle' style={{ color: '#FDB414'}} ></i>
                             <a href="#" className="sidebar-menu-link">My Profile</a>
                         </div>
                     </li>
