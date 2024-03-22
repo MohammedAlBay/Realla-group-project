c619e044b7d0aabc02bca4bc3e2d73622253a27a
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-22 11:31:33 +0100

    add background to payment panel

--- resources/js/Pages/PayNow.jsx
+++ resources/js/Pages/PayNow.jsx
@@ -15,35 +15,35 @@ const PayNow = () => {
 
             <div className='checkout'>
                 <div className='order'>
-                    <h2>Checkout</h2>
-                    <h5>Order #0101</h5>
+                    <h2 className="payment-text">Checkout</h2>
+                    <h5 className="payment-text">Order #0101</h5>
                     <ul className='order-list'>
                         <li>
                             <img
                                 src=''
                                 alt=''/>
-                            <h4>Text</h4>
-                            <h5>€110</h5>
+                            <h4 className="payment-text">Text</h4>
+                            <h5 className="payment-text">€110</h5>
                         </li>
                         <li>
                             <img
                                 src=''
                                 alt=''/>
-                            <h4>Text</h4>
-                            <h5>€28</h5>
+                            <h4 className="payment-text">Text</h4>
+                            <h5 className="payment-text">€28</h5>
                         </li>
                         <li>
                             <img
                                 src=''
                                 alt=''/>
-                            <h4>Text</h4>
-                            <h5>€20</h5>
+                            <h4 className="payment-text">Text</h4>
+                            <h5 className="payment-text">€20</h5>
                         </li>
                     </ul>
-                    <h5>Shipping</h5>
-                    <h4>€ 9.50</h4>
+                    <h5 className="payment-text">Shipping</h5>
+                    <h4 className="payment-text">€ 9.50</h4>
                     <h5 className='total'>Total</h5>
-                    <h1>€ 167.50  </h1>
+                    <h1 className="payment-text">€ 167.50  </h1>
                 </div>
                 <h2>Payment</h2>
                 <div id='payment' className='payment'>
@@ -54,7 +54,7 @@ const PayNow = () => {
                                  xmlnsXlink='http://www.w3.org/1999/xlink'>
                                 {/* Add your SVG paths here */}
                             </svg>
-                            <h5>Card Number</h5>
+                            <h5 className="payment-text">Card Number</h5>
                             <h6 id='label-cardnumber'>0000 0000 0000 0000</h6>
                             <h5>Expiration<span>CVC</span></h5>
                             <h6 id='label-cardexpiration'>00 / 0000<span>000</span></h6>
