3a7de495e4dfa922acef4cb6bbe6106021ee5fce
Jana2xN <jana.neirinck@hotmail.com>
2024-03-22 09:06:29 +0100

    Place LoginOptionsImage higher on page

--- resources/js/Pages/LoginOptions.jsx
+++ resources/js/Pages/LoginOptions.jsx
@@ -8,7 +8,7 @@ const LoginOptions = () => {
                 <div style={{ position: 'relative', width: '500px', height: '725px', flexShrink: 0, borderRadius: '0px 0px 100px 100px', border: '3px solid rgba(0, 0, 0, 0.00)', background: '#1E2127' }}></div>
             </div>
             <div className="absolute inset-0 flex justify-start items-center" style={{ marginLeft: '6rem', zIndex: '1' }}> {/* Ensure this div is above the buttons */}
-                <img src="/images/LoginOptionsImage.png" alt="LoginOptions" className="object-cover w-3/5 h-auto"/> 
+                <img src="/images/LoginOptionsImage.png" alt="LoginOptions" className="object-cover w-3/5 h-auto" style={{ marginTop: '-4rem' }}/> 
             </div>
             <div className="flex justify-center items-center">
                 <div className="flex flex-col justify-center items-center mr-28" style={{ zIndex: '2' }}> {/* Ensure this div is above the image */}
