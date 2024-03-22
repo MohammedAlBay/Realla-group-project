1de37083513cd10a0d7cfd69e368c0cc3d265972
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-22 09:40:27 +0100

    update payment form

--- resources/js/Pages/PayNow.jsx
+++ resources/js/Pages/PayNow.jsx
@@ -17,30 +17,30 @@ const PayNow = () => {
                     <ul className='order-list'>
                         <li>
                             <img
-                                src='http://images.nike.com/is/image/DotCom/PDP_THUMB/JORDAN-ECLIPSE-724010_401_A_PREM.jpg'
-                                alt='Jordan Eclipse'/>
-                            <h4>Jordan Eclipse</h4>
-                            <h5>$110</h5>
+                                src=''
+                                alt=''/>
+                            <h4>Text</h4>
+                            <h5>€110</h5>
                         </li>
                         <li>
                             <img
-                                src='http://images.nike.com/is/image/DotCom/PDP_THUMB/Nike-Futura-True-2-Adjustable-Hat-584169_451_A.jpg'
-                                alt='Nike Futura True 2 Snapback'/>
-                            <h4>Nike Futura True 2 Snapback</h4>
-                            <h5>$28</h5>
+                                src=''
+                                alt=''/>
+                            <h4>Text</h4>
+                            <h5>€28</h5>
                         </li>
                         <li>
                             <img
-                                src='http://images.nike.com/is/image/DotCom/PDP_THUMB/NIKE-FB-SHOE-BAG-30-BA5101_001_A.jpg'
-                                alt='Nike FB Training 3.0'/>
-                            <h4>Nike FB Training 3.0</h4>
-                            <h5>$20</h5>
+                                src=''
+                                alt=''/>
+                            <h4>Text</h4>
+                            <h5>€20</h5>
                         </li>
                     </ul>
                     <h5>Shipping</h5>
-                    <h4>$ 9.50</h4>
+                    <h4>€ 9.50</h4>
                     <h5 className='total'>Total</h5>
-                    <h1>$ 167.50</h1>
+                    <h1>€ 167.50</h1>
                 </div>
                 <h2>Payment</h2>
                 <div id='payment' className='payment'>
