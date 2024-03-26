5f7adc4d49bacd1b5a414cbe69fb329ccbd12011
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-26 10:44:38 +0100

    change the color of events of calendar

--- resources/js/Pages/CalendarTenant.jsx
+++ resources/js/Pages/CalendarTenant.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import '../../css/Appointment.css'
 
 const localizer = momentLocalizer(moment);
 
@@ -20,14 +21,27 @@ function CalendarTenant() {
         },
     ];
 
+    // Event style function to customize event content color
+    const eventStyleGetter = (event, start, end, isSelected) => {
+        const style = {
+            backgroundColor: '#FDB514',
+            color: 'white',
+            borderRadius: '5px',
+            border: 'none',
+        };
+        return {
+            style,
+        };
+    };
+
     return (
-        <div>
-           
+        <div className="calendar-container">
             <Calendar
                 localizer={localizer}
                 events={events}
                 startAccessor="start"
                 endAccessor="end"
+                eventPropGetter={eventStyleGetter}
                 style={{ height: 500, width: 750, backgroundColor: 'white', color: 'darkgray' }}
             />
         </div>
