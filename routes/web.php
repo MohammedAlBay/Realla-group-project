ee9e9f802c967506eb3f794d6ec65c65e1c5cab9
FundaErmete <funda.ermete@gmail.com>
2024-03-26 15:13:39 +0100

    Update web.php file

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
