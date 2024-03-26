d096f84fbe240b7ffb2b2b3d988844a930896c5d
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-26 15:38:14 +0100

    update web.php file

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,6 +16,7 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 /*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -159,4 +161,3 @@ Route::get('/reportproblem', function () {
 Route::get('/{any}', function () {
     return Inertia::render('CustomErrorPage');
 })->where('any','.*');
-
