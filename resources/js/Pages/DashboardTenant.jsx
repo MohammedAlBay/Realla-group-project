45e16a4210e08d25890fd4bb2fd92feccbd5cdba
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-27 13:18:11 +0100

    dashboard bar problem solved

--- resources/js/Pages/DashboardTenant.jsx
+++ resources/js/Pages/DashboardTenant.jsx
@@ -1,57 +1,17 @@
 // DashboardTenant.js
+import React from 'react';
 
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "../../css/Dashboard.css";
-import DashboardTenantBar from "@/Components/DashboardTenantBar.jsx";
 import DashboardTenantLanding from "@/Pages/DashboardTenantLanding.jsx";
-import PayNow from "@/Pages/PayNow.jsx";
-import Mailbox from "@/Pages/MailboxTenant";
-import Documents from "@/Pages/Documents.jsx";
-import Gallery from "@/Pages/Gallery.jsx";
-import AppointmentTenant from "@/Pages/AppointmentTenant.jsx";
-import ReportProblem from "@/Pages/ReportProblem.jsx";
 
 const DashboardTenant = () => {
-    // State to track the current page
-    const [currentPage, setCurrentPage] = useState('dashboard-tenant');
-
-    // Function to handle page change
-    const handlePageChange = (page) => {
-        setCurrentPage(page);
-    };
-
-    // Render the current page/component based on the state
-    const renderPage = () => {
-        switch (currentPage) {
-            case 'dashboard-tenant':
-                return <DashboardTenantLanding />;
-            case 'gallery':
-                return <Gallery />;
-            case 'reportproblem':
-                return <ReportProblem />;
-            case 'mailbox-tenant':
-                return <Mailbox />;
-            case 'paynow':
-                return <PayNow />;
-            case 'documents':
-                return <Documents />;
-            case 'appointment-tenant':
-                return <AppointmentTenant />;
-            default:
-                return <DashboardTenantLanding />;
-        }
-    };
-
     return (
-        <Router>
             <div className="dashboard">
-                <DashboardTenantBar onPageChange={handlePageChange} />
+
                 <div className="content">
-                    {renderPage()}
+                    <DashboardTenantLanding />
                 </div>
             </div>
-        </Router>
     );
 };
 
--- resources/js/Pages/DashboardTenantLanding.jsx
+++ resources/js/Pages/DashboardTenantLanding.jsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import "../../css/Dashboard.css"
-import CircularSliderCarousel from "@/Components/CircularSliderCarousel.jsx";
+// DashboardTenant.js
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "../../css/Dashboard.css";
 import DashboardTenantBar from "@/Components/DashboardTenantBar.jsx";
 
-
-
+import PayNow from "@/Pages/PayNow.jsx";
+import Mailbox from "@/Pages/MailboxTenant";
+import Documents from "@/Pages/Documents.jsx";
+import Gallery from "@/Pages/Gallery.jsx";
+import AppointmentTenant from "@/Pages/AppointmentTenant.jsx";
+import ReportProblem from "@/Pages/ReportProblem.jsx";
+import CircularSliderCarousel from "@/Components/CircularSliderCarousel.jsx";
 
 const DashboardTenantLanding = () => {
+    // State to track the current page
+    const [currentPage, setCurrentPage] = useState('dashboard-tenant');
+
+    // Function to handle page change
+    const handlePageChange = (page) => {
+        setCurrentPage(page);
+    };
+
+    // Render the current page/component based on the state
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'dashboard-tenant':
+                return <CircularSliderCarousel/>;
+            case 'gallery':
+                return <Gallery />;
+            case 'reportproblem':
+                return <ReportProblem />;
+            case 'mailbox-tenant':
+                return <Mailbox />;
+            case 'paynow':
+                return <PayNow />;
+            case 'documents':
+                return <Documents />;
+            case 'appointment-tenant':
+                return <AppointmentTenant />;
+            default:
+                return < CircularSliderCarousel  />;
+        }
+    };
+
     return (
-        <div className="dashboard">
-
-            <DashboardTenantBar />
-
-            <div className="content">
-                 <CircularSliderCarousel />
+        <Router>
+            <div className="dashboard">
+                <DashboardTenantBar onPageChange={handlePageChange} />
+                <div className="content">
+                    {renderPage()}
+                </div>
             </div>
-
-
-        </div>
+        </Router>
     );
 };
-export default DashboardTenantLanding;
 
+export default DashboardTenantLanding;
