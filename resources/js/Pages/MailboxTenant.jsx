3bd1ebf813923c213ed5defe1abaf8286d0e65e8
Jana2xN <jana.neirinck@hotmail.com>
2024-03-28 15:19:14 +0100

    Changed margin top for forgot password link on both login panels

--- resources/js/Pages/MailboxTenant.jsx
+++ resources/js/Pages/MailboxTenant.jsx
@@ -8,13 +8,13 @@ const MailboxTenant = () => {
     
     return (
         <div className="relative h-screen flex justify-center items-center">
-            <div className="mr-8 w-4/5 h-2/3 flex-shrink-0 rounded-xl bg-white shadow-lg relative" style={{ width: '80%', boxShadow: '0 12px 16px rgba(0.1, 0.1, 0.1, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08)' }}>
+            <div className="mr-8 w-4/5 h-2/3 flex-shrink-0 rounded-xl bg-white shadow-lg relative" style={{ boxShadow: '0 12px 16px rgba(0.1, 0.1, 0.1, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08)' }}>
                 <div style={{ padding: '20px' }}>
                     <h3 style={{ marginTop: '10px', marginLeft: '10px' }}>Message</h3>
                 </div>
-                <div class="message-role">Gardener</div>
-                <div class="message-role">11:15 AM</div>
-                <div class="message-role">Subject</div>
+                <div className="message-role">Gardener</div>
+                <div className="message-role">11:15 AM</div>
+                <div className="message-role">Subject</div>
 
                 <div class="message-role">Cleaner</div>
                 <div class="message-role">11:15 AM</div>
