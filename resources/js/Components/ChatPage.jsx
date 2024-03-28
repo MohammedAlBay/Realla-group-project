41039b977899b5cc85b16f369defd1425fd9d284
Jana2xN <jana.neirinck@hotmail.com>
2024-03-28 15:55:16 +0100

    Add const ChatPage and export default to the ChatPage component

--- resources/js/Components/ChatPage.jsx
+++ resources/js/Components/ChatPage.jsx
@@ -3,3 +3,18 @@ import ChatBar from './ChatBar';
 import ChatBody from './ChatBody';
 import ChatFooter from './ChatFooter';
 
+const ChatPage = () => {
+    return (
+        <div className="chat__container">
+            <div className="chat__sidebar">
+                <ChatBar />
+            </div>
+            <div className="chat__main">
+                <ChatBody />
+                <ChatFooter />
+            </div>
+        </div>
+    );
+};
+
+export default ChatPage;
