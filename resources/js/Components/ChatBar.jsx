c0e10c4bf38b994c82adecdd62384a94553e07de
Jana2xN <jana.neirinck@hotmail.com>
2024-03-28 15:57:02 +0100

    Add import, const ChatBar and export default to ChatBar component

---
+++ resources/js/Components/ChatBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChatBar = () => {
+    return (
+        <div className="chat__bar">
+            <h2>Active Users</h2>
+            {/* List of active users */}
+        </div>
+    );
+};
+
+export default ChatBar;
