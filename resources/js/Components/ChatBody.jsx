3e60279079cf04a29f07cd767eca59604722a1b7
Jana2xN <jana.neirinck@hotmail.com>
2024-03-28 15:58:29 +0100

    Add import, const ChatBar and export default to ChatBody component

---
+++ resources/js/Components/ChatBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChatBody = () => {
+    return (
+        <div className="chat__body">
+            {/* List of messages */}
+        </div>
+    );
+};
+
+export default ChatBody;
