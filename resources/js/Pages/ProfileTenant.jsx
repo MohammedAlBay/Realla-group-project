82f338aa86356a46d910b7bfdee85f17f9a7f096
MohammedAB <albay.m@hotmail.com>
2024-03-29 16:15:57 +0100

    Remove import

--- resources/js/Pages/ProfileTenant.jsx
+++ resources/js/Pages/ProfileTenant.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import DashboardTenantBar from "@/Components/DashboardTenantBar.jsx";
 import PropertyBookmarks from "@/Components/PropertyBookmarks.jsx";
 import ProfileInfo from "@/Components/ProfileInfo.jsx";
 import '../../css/Profile.css';
