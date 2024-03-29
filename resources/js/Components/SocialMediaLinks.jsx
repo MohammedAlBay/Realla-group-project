030bd2b174348f514431be4948f9f35a8c2c5ed2
MohammedAB <albay.m@hotmail.com>
2024-03-29 10:20:25 +0100

    Change function to arrow function

--- resources/js/Components/SocialMediaLinks.jsx
+++ resources/js/Components/SocialMediaLinks.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function SocialMediaLinks() {
+const SocialMediaLinks = () => {
   return (
     <ul className="social-media-links">
       <li><a href="#"><i class='bx bxl-facebook-circle' style={{color:'#fdb514'}}></i></a></li>
