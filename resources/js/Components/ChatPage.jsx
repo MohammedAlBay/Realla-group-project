ebcc9708ad9a56d907815fec62a7d1e13500a049
Jana2xN <jana.neirinck@hotmail.com>
2024-03-29 09:18:57 +0100

    Update ChatPage component

--- resources/js/Components/ChatPage.jsx
+++ resources/js/Components/ChatPage.jsx
@@ -5,16 +5,15 @@ import ChatFooter from './ChatFooter';
 
 const ChatPage = () => {
     return (
-        <div className="chat__container">
-            <div className="chat__sidebar">
-                <ChatBar />
-            </div>
-            <div className="chat__main">
-                <ChatBody />
-                <ChatFooter />
-            </div>
+        <div className="chat">
+            <ChatBar />
+        <div className="chat__main">
+            <ChatBody />
+            <ChatFooter />
+        </div>
         </div>
     );
 };
+
 
 export default ChatPage;
