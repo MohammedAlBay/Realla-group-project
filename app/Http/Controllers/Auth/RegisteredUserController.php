66a701871a7e578c818c64575d6a19cd810892bf
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-29 12:25:28 +0100

    fix dashboard styling problems

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -47,6 +47,6 @@ class RegisteredUserController extends Controller
 
         Auth::login($user);
 
-        return redirect('dashboard-tenant');
+        return redirect('/dashboard-tenant');
     }
 }
