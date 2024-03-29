437f5efa9e74dad547924b7e78ac2a8496b5e9cb
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-29 11:40:25 +0100

    Revert "update register page"
    This reverts commit 51468a080876fa16b9b350c3718b09999dc44bc4.

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,5 +1,4 @@
 import { useForm } from '@inertiajs/inertia-react';
-import { useState } from 'react';
 
 const Register = () => {
     const { data, setData, post, processing, errors } = useForm({
@@ -9,35 +8,21 @@ const Register = () => {
         password_confirmation: '',
     });
 
-    const [submitting, setSubmitting] = useState(false);
-
     const handleSubmit = (e) => {
         e.preventDefault();
-        setSubmitting(true);
-        post('/register', {
-            onSuccess: () => {
-                // Handle successful registration, e.g., redirect user
-                // or display success message.
-            },
-            onError: () => {
-                setSubmitting(false);
-            }
-        });
+        post('/register');
     };
 
     return (
         <form onSubmit={handleSubmit}>
             <input type="text" value={data.name} onChange={(e) => setData('name', e.target.value)} />
-            {errors.name && <span>{errors.name}</span>} {/* Display name validation error */}
             <input type="email" value={data.email} onChange={(e) => setData('email', e.target.value)} />
-            {errors.email && <span>{errors.email}</span>} {/* Display email validation error */}
             <input type="password" value={data.password} onChange={(e) => setData('password', e.target.value)} />
-            {errors.password && <span>{errors.password}</span>} {/* Display password validation error */}
             <input type="password" value={data.password_confirmation} onChange={(e) => setData('password_confirmation', e.target.value)} />
-            {errors.password_confirmation && <span>{errors.password_confirmation}</span>} {/* Display password confirmation validation error */}
-            <button type="submit" disabled={processing || submitting}>Register</button>
+            <button type="submit" disabled={processing}>Register</button>
         </form>
     );
 };
 
 export default Register;
+
