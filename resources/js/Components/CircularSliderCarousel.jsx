3440bf169ff2a2147b890596d87ec1214e2c838f
sezin akbulut <sezin.akbulut@gmail.com>
2024-03-31 20:06:05 +0200

    add shadow mask to backgorund of idashboard img

--- resources/js/Components/CircularSliderCarousel.jsx
+++ resources/js/Components/CircularSliderCarousel.jsx
@@ -33,11 +33,14 @@ const CircularSliderCarousel = () => {
 
             <h1 className="text-dashboard-landing">Welcome <span>Olivia Janssens {/*{userAddress}*/} </span>!</h1>
             {/* Render user address with dynamic value */}
-            <h4 className="address-dashboard-landing"><i class='bx bxs-map' style={{color: '#FDB414', fontSize: '30px'}}></i> <span>Sint Niklass {userAddress} </span> </h4>
+            <h4 className="address-dashboard-landing"><i class='bx bxs-map'
+                                                         style={{color: '#FDB414', fontSize: '30px'}}></i> <span>Sint Niklass {userAddress} </span>
+            </h4>
 
             <div className="home-img-dashboard">
                 <img src="./images/Mask Group.png" alt="Bungalow"/>
             </div>
+            <div className="Shadow2"></div>
         </div>
     );
 };
