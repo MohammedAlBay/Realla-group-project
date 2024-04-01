fd6d7fac4e7d861a7a7af29700ea45314ff016f5
FundaErmete <funda.ermete@gmail.com>
2024-04-01 23:43:03 +0200

    Modify onClick function

--- resources/js/Pages/ReportProblem.jsx
+++ resources/js/Pages/ReportProblem.jsx
@@ -16,7 +16,7 @@ const problems = [
   { id: 12, icon: '/images/toilet.png', alt: 'Leak' },
 ];
 
-const ReportProblem = () => {
+const ReportProblem = ({ onFollowUpClick }) => {
   const [currentPage, setCurrentPage] = useState(0);
   const problemsPerPage = 4;
 
@@ -26,7 +26,6 @@ const ReportProblem = () => {
 
   const goToNextPage = () => {
     setCurrentPage((prevPage) => {
-    
       if (indexOfLastProblem >= problems.length) {
         return 0;
       } else {
@@ -37,7 +36,6 @@ const ReportProblem = () => {
   
   const goToPrevPage = () => {
     setCurrentPage((prevPage) => {
-     
       if (prevPage === 0) {
         return Math.ceil(problems.length / problemsPerPage) - 1;
       } else {
@@ -66,8 +64,8 @@ const ReportProblem = () => {
           <div className="flex justify-center space-x-4 mx-16">
             {currentProblems.map((problem, index) => (
               <div key={problem.id} className="flex-shrink-0 w-28 h-28 m-2 rounded-full bg-gray-700 flex flex-col items-center justify-center">
-                <img src={problem.icon} alt={problem.label} className="w-full h-full object-cover" />
-                <span className="text-sm text-white">{problem.label}</span>
+                <img src={problem.icon} alt={problem.alt} className="w-full h-full object-cover" />
+                <span className="text-sm text-white">{problem.alt}</span>
               </div>
             ))}
           </div>
@@ -75,27 +73,26 @@ const ReportProblem = () => {
           <button onClick={goToNextPage} className="flex items-center justify-center p-0 rounded-full w-8 h-8 bg-yellow-600 text-white-500" style={{ backgroundColor: '#fdb514' }} aria-label="Next problem">&gt;</button>
         </div>
 
-    <form onSubmit={handleSubmit} className="mt-12">
-        <div className="mb-10 mx-8 px-6">
+        <form onSubmit={handleSubmit} className="mt-12">
+          <div className="mb-10 mx-8 px-6">
             <textarea id="problem" value={problemDescription} onChange={(e) => setProblemDescription(e.target.value)} rows="4" className="w-full p-2 border border-gray-600 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500 placeholder-gray-400 text-gray-600" placeholder="Provide a detailed description of the problem..."></textarea>
-        </div>
+          </div>
 
-        <div className="flex justify-between items-center w-full">
-  
-        <div className="flex items-center mx-14 mt-[-24px]">
-          <button type="button" className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold rounded-full w-8 h-8 flex items-center justify-center" style={{ backgroundColor: '#fdb514' }} aria-label="Add a photo">
-            <span className="text-xl">+</span>
-          </button>
-            <span className="text-white font-bold ml-4">Add a photo</span>
-        </div>
+          <div className="flex justify-between items-center w-full">
+            <div className="flex items-center mx-14 mt-[-24px]">
+              <button type="button" className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold rounded-full w-8 h-8 flex items-center justify-center" style={{ backgroundColor: '#fdb514' }} aria-label="Add a photo">
+                <span className="text-xl">+</span>
+              </button>
+              <span className="text-white font-bold ml-4">Add a photo</span>
+            </div>
 
-        <div className="flex space-x-4 mx-14 mt-[-24px]">
-          <button type="submit" className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-10 rounded-lg" style={{ backgroundColor: '#fdb514' }}>Send</button>
-          <button type="button" className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-6 rounded-lg" style={{ backgroundColor: '#fdb514' }}>Follow up</button>
-        </div>
-    </div>
-    </form>
-    </div>
+            <div className="flex space-x-4 mx-14 mt-[-24px]">
+              <button type="submit" className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-10 rounded-lg" style={{ backgroundColor: '#fdb514' }}>Send</button>
+              <button type="button" onClick={onFollowUpClick} className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-6 rounded-lg" style={{ backgroundColor: '#fdb514' }}>Follow up</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
