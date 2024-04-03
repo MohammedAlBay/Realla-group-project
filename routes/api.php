f972a791c0871e110086c16b879b524948b3dc6c
MohammedAB <albay.m@hotmail.com>
2024-04-03 15:56:41 +0200

    Define route for the CRUD operations for bookmarks API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\BookmarkController;
+
+
+
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -22,6 +28,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //DATABASE
 Route::get('/properties', [PropertyController::class, 'index']);
-/*
-Route::get('/properties', [PropertyController::class, 'store']);
-*/
+
+// CRUD operations for bookmarks API
+Route::get('/bookmarks', [BookmarkController::class, 'index']);
+Route::get('/bookmarks/{id}', [BookmarkController::class, 'show']);
+Route::post('/bookmarks', [BookmarkController::class, 'store']);
+Route::put('/bookmarks/{id}', [BookmarkController::class, 'update']);
+Route::delete('/bookmarks/{id}', [BookmarkController::class, 'destroy']);
