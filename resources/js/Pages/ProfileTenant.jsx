1fb65b58ae5c8d0ecbf0b514524560bdb5a5c390
MohammedAB <albay.m@hotmail.com>
2024-04-03 16:16:05 +0200

    Import Link component, Add 'Back' button & Wrap inside profile-container div

--- resources/js/Pages/ProfileTenant.jsx
+++ resources/js/Pages/ProfileTenant.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropertyBookmarks from "@/Components/PropertyBookmarks.jsx";
 import ProfileInfo from "@/Components/ProfileInfo.jsx";
+import { Link } from '@inertiajs/react';
 import '../../css/Profile.css';
 
 const ProfileTenant = () => {
   return(
     <>
-      <ProfileInfo />
-      <PropertyBookmarks />
+      <div className="profile-container">
+        <div className="heading"><h2>My Profile</h2></div>
+        <ProfileInfo />
+        <PropertyBookmarks />
+        <button className="back-button"><Link href="/dashboard-tenant">Back</Link></button>
+      </div>
     </>
   );
 }
