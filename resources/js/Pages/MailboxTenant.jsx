861311c4f6a1189bbbb81a5a3a2b550d91d28570
Jana2xN <jana.neirinck@hotmail.com>
2024-04-05 04:49:08 +0200

    Add background to gardener in the contact list in mailbox

--- resources/js/Pages/MailboxTenant.jsx
+++ resources/js/Pages/MailboxTenant.jsx
@@ -20,7 +20,7 @@ const MailboxTenant = () => {
                         {/* Input */}
                         <input type="text" placeholder="Write message down here ..." style={{ fontSize: '0.85rem', marginBottom: '20px', marginLeft: '20px', padding: '5px', width: '50%', height: '35px', borderRadius: '5px', border: '1px solid #ccc', position: 'absolute', left: '35%', bottom: '5px' }} />
                         {/* Profile picture */}
-                        <div style={{ width: '42px', height: '42px', flexShrink: 0, borderRadius: '50%', border: '3px solid rgba(0, 0, 0, 0.00)', position: 'absolute', left: 'calc(52% - 120px)', top: '15px', backgroundImage: 'url(/images/gardener.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
+                        <div style={{ width: '42px', height: '42px', flexShrink: 0, borderRadius: '50%', border: '3px solid rgba(0, 0, 0, 0.00)', position: 'absolute', left: 'calc(52% - 120px)', top: '15px', backgroundImage: 'url(/images/MailboxTenantGardener.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}></div>
                         {/* Green dot and Online text */}
                         <div style={{ fontSize: '0.85rem', display: 'flex', alignItems: 'center', position: 'absolute', left: 'calc(59% - 120px)', top: '35px' }}>
                             <div style={{ width: '10px', height: '10px', borderRadius: '50%', background: 'green', marginRight: '5px' }}></div>
@@ -35,72 +35,75 @@ const MailboxTenant = () => {
                         <div style={{ position: 'absolute', left: '35%', right: 0, bottom: '87.3%', height: '1px', background: '#ccc' }}></div>
                         {/* Horizontal grey line bottom */}
                         <div style={{ position: 'absolute', left: '35%', right: 0, bottom: '15%', height: '1px', background: '#ccc' }}></div>
-
                         {/* First textblock chat left */}
-                        <div style={{ width: '220px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 20px 0px', border: 'none', fontSize: '0.85rem', backgroundColor: '#C2C2C2', position: 'absolute', left: '38%', bottom: '75%', padding: '5px 15px' }}>What can we do for you?</div>
+                        <div style={{ width: '220px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 20px 0px', border: 'none', fontSize: '0.85rem', backgroundColor: '#E5E5E5', position: 'absolute', left: '38%', bottom: '75%', padding: '5px 15px' }}>What can we do for you?</div>
                         <span style={{ fontSize: '0.60rem', marginLeft: '250px', position: 'absolute', top: '21.5%' }}>11:15</span>
                         {/* Second textblock chat left */}
-                        <div style={{ width: '215px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 20px 0px', border: 'none', fontSize: '0.85rem', backgroundColor: '#C2C2C2', position: 'absolute', left: '38%', bottom: '50%', padding: '5px 15px' }}>What needs to be done?</div>
-                        <span style={{ fontSize: '0.60rem', marginLeft: '245px', position: 'absolute', top: '46.5%' }}>11:15</span>                      
+                        <div style={{ width: '215px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 20px 0px', border: 'none', fontSize: '0.85rem', backgroundColor: '#E5E5E5', position: 'absolute', left: '38%', bottom: '50%', padding: '5px 15px' }}>What needs to be done?</div>
+                        <span style={{ fontSize: '0.60rem', marginLeft: '245px', position: 'absolute', top: '46.5%' }}>11:40</span>                      
                         {/* Third textblock chat left*/}
-                        <div style={{ width: '255px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 20px 0px', border: 'none', fontSize: '0.85rem', backgroundColor: '#C2C2C2', position: 'absolute', left: '38%', bottom: '18%', padding: '5px 15px' }}>This will be done by next week.</div>                  
-                        <span style={{ fontSize: '0.60rem', marginLeft: '285px', position: 'absolute', top: '78.5%' }}>11:15</span>                                              
+                        <div style={{ width: '255px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 20px 0px', border: 'none', fontSize: '0.85rem', backgroundColor: '#E5E5E5', position: 'absolute', left: '38%', bottom: '18%', padding: '5px 15px' }}>This will be done by next week.</div>                  
+                        <span style={{ fontSize: '0.60rem', marginLeft: '285px', position: 'absolute', top: '78.5%' }}>12:17</span>                                              
                         {/* First textblock chat right*/}
-                        <div style={{ width: '200px', height: '60px', flexShrink: 0, borderRadius: '20px 20px 0px 20px', border: 'none', fontSize: '0.85rem', backgroundColor: '#C2C2C2', position: 'absolute', left: '71.5%', bottom: '60%', padding: '10px 15px' }}>I'm contacting you for some lawn maintenance.</div>                   
-                        <span style={{ fontSize: '0.60rem', marginLeft: '488px', position: 'absolute', top: '36.3%' }}>11:15</span>                                              
+                        <div style={{ width: '200px', height: '60px', flexShrink: 0, borderRadius: '20px 20px 0px 20px', border: 'none', fontSize: '0.85rem', backgroundColor: '#FFF0CF', position: 'absolute', left: '71.5%', bottom: '60%', padding: '10px 15px' }}>I'm contacting you for some lawn maintenance.</div>                   
+                        <span style={{ fontSize: '0.60rem', marginLeft: '492px', position: 'absolute', top: '36.3%' }}>11:32</span>                                              
                         {/* Second textblock chat right*/}
-                        <div style={{ width: '220px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 0px 20px', border: 'none', fontSize: '0.85rem', backgroundColor: '#C2C2C2', position: 'absolute', left: '69%', bottom: '40%', padding: '5px 15px' }}> Mowing, edging and watering.</div>   
+                        <div style={{ width: '245px', height: '30px', flexShrink: 0, borderRadius: '20px 20px 0px 20px', border: 'none', fontSize: '0.85rem', backgroundColor: '#FFF0CF', position: 'absolute', left: '65.5%', bottom: '40%', padding: '5px 15px' }}> Mowing, edging and watering.</div>   
+                        <span style={{ fontSize: '0.60rem', marginLeft: '492px', position: 'absolute', top: '56.7%' }}>11:56</span>                                                                      
                         {/* Image left, chat right*/}
-                        <div style={{ width: '100px', height: '60px', flexShrink: 0, borderRadius: '15px', border: 'none', backgroundColor: '#C2C2C2', position: 'absolute', left: '69%', bottom: '25%' }}></div>  
+                        <div style={{ width: '100px', height: '60px', flexShrink: 0, borderRadius: '15px', border: 'none', position: 'absolute', left: '69%', bottom: '27%' }}>
+                            <img src="/images/MailboxTenantGrass.jpg" alt="Grass" style={{ width: '100%', height: '100%', borderRadius: '15px' }} />
+                        </div>  
                         {/* Image right, chat right*/}
-                        <div style={{ width: '100px', height: '60px', flexShrink: 0, borderRadius: '15px', border: 'none', backgroundColor: '#C2C2C2', position: 'absolute', left: '84%', bottom: '25%' }}></div>                    
+                        <div style={{ width: '100px', height: '60px', flexShrink: 0, borderRadius: '15px', border: 'none', position: 'absolute', left: '84%', bottom: '27%' }}>
+                            <img src="/images/MailboxTenantEdging.jpg" alt="Grass" style={{ width: '100%', height: '100%', borderRadius: '15px' }} />                        
+                        </div>                    
                     </div>
-                    <div style={{ marginLeft: '30px', display: 'flex', flexDirection: 'column', fontSize: '0.85rem', marginBottom: '-10px' }}>
-
+                    <div style={{ display: 'flex', flexDirection: 'column', fontSize: '0.85rem', marginBottom: '-10px' }}>
                         <div className='flex'>
-                            <div className="message-role" style={{ marginBottom: '5px' }}>
+                            <div className="message-role" style={{ backgroundColor: '#FDB514', borderRadius: '20px', padding: '10px 10px 3px 10px', marginBottom: '5px', marginLeft: '20px' }}>
                             Gardener
-                            <span style={{ marginLeft: '90px' }}>11:15 AM</span>
+                            <span style={{ fontSize: '0.70rem', marginLeft: '98px' }}>12:17 AM</span>
                             <div className="message-role" style={{ marginTop: '5px', marginBottom: '10px' }}>What’s the progress on that task?</div>
                         </div>
                         </div>
 
                         <div className='flex'>
-                            <div className="message-role" style={{ marginTop: '15px', marginBottom: '5px' }}>
+                            <div className="message-role" style={{ marginLeft: '30px', marginTop: '15px', marginBottom: '5px' }}>
                             Cleaner
-                            <span style={{ marginLeft: '100px' }}>11:15 AM</span>
+                            <span style={{ fontSize: '0.70rem', marginLeft: '108px' }}>11:09 AM</span>
                             <div className="message-role" style={{ marginBottom: '10px' }}>What’s the progress on that task?</div>
                         </div>
                         </div>
 
                         <div className='flex'>
-                            <div className="message-role" style={{ marginTop: '15px', marginBottom: '5px' }}>
+                            <div className="message-role" style={{ marginLeft: '30px', marginTop: '15px', marginBottom: '5px' }}>
                             Landlord
-                            <span style={{ marginLeft: '95px' }}>11:15 AM</span>
+                            <span style={{ fontSize: '0.70rem', marginLeft: '103px' }}>19:32 AM</span>
                             <div className="message-role" style={{ marginBottom: '10px' }}>What’s the progress on that task?</div>
                         </div>
                         </div>
 
                         <div className='flex'>
-                            <div className="message-role" style={{ marginTop: '15px', marginBottom: '5px' }}>
+                            <div className="message-role" style={{ marginLeft: '30px', marginTop: '15px', marginBottom: '5px' }}>
                             Painter
-                            <span style={{ marginLeft: '105px' }}>11:15 AM</span>
+                            <span style={{ fontSize: '0.70rem', marginLeft: '112px' }}>11:15 AM</span>
                             <div className="message-role" style={{ marginBottom: '10px' }}>What’s the progress on that task?</div>
                         </div>
                         </div>
 
                         <div className='flex'>
-                            <div className="message-role" style={{ marginTop: '15px', marginBottom: '5px' }}>
+                            <div className="message-role" style={{ marginLeft: '30px', marginTop: '15px', marginBottom: '5px' }}>
                             Repair Man
-                            <span style={{ marginLeft: '78px' }}>11:15 AM</span>
+                            <span style={{  fontSize: '0.70rem', marginLeft: '83px' }}>16:43 AM</span>
                             <div className="message-role" style={{ marginBottom: '10px' }}>What’s the progress on that task?</div>
                         </div>
                         </div>
 
                         <div className='flex'>
-                            <div className="message-role" style={{ marginTop: '15px', marginBottom: '5px' }}>
+                            <div className="message-role" style={{ marginLeft: '30px', marginTop: '15px', marginBottom: '5px' }}>
                             Electrician
-                            <span style={{ marginLeft: '85px' }}>11:15 AM</span>
+                            <span style={{ fontSize: '0.70rem', marginLeft: '93px' }}>10:26 AM</span>
                             <div className="message-role" style={{ marginBottom: '10px' }}>What’s the progress on that task?</div>
                         </div>
                         </div>
@@ -108,9 +111,6 @@ const MailboxTenant = () => {
                             <button style={{ padding: '5px 10px', fontSize: '0.75rem', borderRadius: '20px', border: 'none', cursor: 'pointer', backgroundColor: '#FDB514', color: 'white' }}>Send</button>
                         </div>
                     </div> 
-                    <div>
-                        {/* Other content goes here */}
-                    </div>
                 </div>
             </div>
         </div>
